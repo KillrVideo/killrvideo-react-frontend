7878bf2c098480946fa748e961bd802fb09ca09f
Aaron Ploetz <aaron.ploetz@ibm.com>
2026-03-31 13:24:01 -0500

    updating vite to pull from env

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(() => ({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'https://localhost:7264',
+        target: process.env.VITE_BACKEND_URL,
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path,
